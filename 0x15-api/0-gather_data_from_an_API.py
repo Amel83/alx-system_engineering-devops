49cdbf402d1294ef2ee5e4069cfe2666f610b947
amel83 <tesfaneshk222@gmail.com>
2023-10-23 05:58:39 -0700

    618

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,39 +1,14 @@
 #!/usr/bin/python3
-#it it is meant to be it will bei
-
-
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-def get_employee_todo_progress(employee_id):
-    base_url = 'https://jsonplaceholder.typicode.com'
-    
-    user_response = requests.get(f'{base_url}/users/{employee_id}')
-    user_data = user_response.json()
-    
-    if 'name' not in user_data:
-        print(f"Employee with ID {employee_id} not found.")
-        return
-
-    employee_name = user_data['name']
-    
-
-    todos_response = requests.get(f'{base_url}/todos?userId={employee_id}')
-    todos_data = todos_response.json()
-    
-    completed_tasks = [task for task in todos_data if task['completed']]
-    
-    total_tasks = len(todos_data)
-    completed_tasks_count = len(completed_tasks)
-    
-    print(f"Employee {employee_name} is done with tasks({completed_tasks_count}/{total_tasks}):")
-    
-    for task in completed_tasks:
-        print(f"\t{task['title']}")
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
-    else:
-        employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
