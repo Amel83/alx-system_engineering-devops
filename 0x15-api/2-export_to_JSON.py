b68328c75a9f3b7c8cdf51f1ecc3b565dfb6bd83
amel83 <tesfaneshk222@gmail.com>
2023-10-23 07:33:17 -0700

    618

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""Exports to-do list info in JSon format"""
-
-
+"""Exports to-do list information for a given employee ID to JSON format."""
 import json
 import requests
 import sys
 
 if __name__ == "__main__":
-    idd = sys.argv[1]
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users/{}".format(user_id)).json()
+    user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
-    todo = requests.get(url + "todo", params={"userId": idd}).json()
-    with open("{}.json".format(idd), "w") as jsonfile:
-        json.dump({idd: [{
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
                 "task": t.get("title"),
                 "completed": t.get("completed"),
                 "username": username
-            } for t in todo]}, jsonfile)
+            } for t in todos]}, jsonfile)
