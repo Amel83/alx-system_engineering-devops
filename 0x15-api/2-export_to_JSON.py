68ae14a32c08e7d848c5e3cd6ababf767948039c
amel83 <tesfaneshk222@gmail.com>
2023-10-23 19:33:14 -0700

    247

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to JSON format."""
+"""well help me lord this things are confusing"""
 import json
 import requests
 import sys
+
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
@@ -10,7 +11,6 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
-
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump({user_id: [{
                 "task": t.get("title"),
