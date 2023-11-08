7586a6f254a14030579063c3c1d1b0276cb0b723
amel83 <tesfaneshk222@gmail.com>
2023-11-07 20:00:41 -0800

    526

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixes a wensit in the WordPress file make it work
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
